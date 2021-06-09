0e96bdc25141e748a1e58c0e7a103d17493d0d63
Tianxing Wu <tianxing.wu@metatavu.fi>
2021-06-09 14:13:43 +0300

    temp

---
+++ src/components/locations/saved-locations/saved-locations.tsx
@@ -0,0 +1,200 @@
+import { Avatar, Button, IconButton, List, ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText, Toolbar, Typography, withStyles, WithStyles } from "@material-ui/core";
+import React from "react";
+import { Route } from "../../../generated/client";
+import LogoIcon from "../../../resources/svg/logo-icon";
+import { styles } from "./saved-locations.styles";
+import strings from "../../../localization/strings";
+import DeleteIcon from "@material-ui/icons/DeleteForeverOutlined";
+import ConfirmDialog from "../../generic/dialogs/confirm-dialog";
+
+/**
+ * Interface describing component props
+ */
+interface Props extends WithStyles<typeof styles> {
+  savedRoutes?: Route[];
+  showSavedRoutes?: boolean;
+  onDeleteUserSavedRoute: (routeId: string) => void;
+  onUserRouteSelect: (route: Route) => void;
+}
+
+/**
+ * Interface describing component state
+ */
+interface State {
+  showAllUserLocations: boolean;
+  locationDeleteInitiated: boolean,
+  locationToDelete?: Route
+}
+
+/**
+ * Component for saved locations listing
+ */
+class SavedLocations extends React.Component<Props, State> {
+
+  /**
+   * Component constructor
+   * 
+   * @param props
+   */
+  constructor (props: Props) {
+    super(props);
+    this.state = {
+      showAllUserLocations: false,
+      locationDeleteInitiated: false
+    };
+  }
+
+  /**
+   * Component render method
+   */
+  public render = () => {
+    const { classes } = this.props;
+    const { locationDeleteInitiated } = this.state;
+    return (
+      <>
+        <Toolbar>
+          <Typography variant="h2">{ strings.routes.savedRoutes }</Typography>
+        </Toolbar>
+        <List style={{ paddingTop: 0 }}>
+          { this.renderListItems() }
+        </List>
+        <div className={ classes.showMoreButtonContainer }>
+          <Button 
+            className={ classes.primaryButton }
+            fullWidth
+            variant="contained"
+            onClick={ this.onShowMoreClick }
+          >
+            { this.state.showAllUserLocations ? strings.showLess : strings.showMore }
+          </Button>
+        </div>
+        <ConfirmDialog 
+          title={ strings.deleteConfirm } 
+          positiveButtonText={ strings.common.yes } 
+          cancelButtonText={ strings.common.cancel } 
+          dialogVisible={ routeDeleteInitiated } 
+          onDialogConfirm={ this.onDeleteConfirm } 
+          onDialogCancel={ this.onDeleteCancel } />
+      </>
+    );
+  }
+
+  /**
+   * Returns rendered user saved routes
+   */
+  private renderListItems = () => {
+    const { savedRoutes, showSavedRoutes } = this.props;
+    const { showAllUserLocations } = this.state;
+
+    if (!savedRoutes || !showSavedRoutes) {
+      return;
+    }
+
+    const existingRoutes = savedRoutes.map(route => {
+      if (route.locationFromName && route.locationToName) {
+        return route;
+      }
+    }).filter( (route: Route | undefined): route is Route => !!route );
+
+    const routesToDisplay = showAllUserLocations ? existingRoutes : existingRoutes.splice(0, 2);
+
+    return routesToDisplay.map((route, index) => {
+      const routeName = route.name;
+      const from = this.truncateName(route.locationFromName, 40);
+      const to = this.truncateName(route.locationToName, 40);
+
+      return (
+        <ListItem button key={ index } onClick={ () => this.onListItemClick(route) }>
+          <ListItemAvatar>
+            <Avatar>
+              <LogoIcon htmlColor="#fff" fontSize="small" />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText 
+            primary={ routeName }
+            secondary={ `${ from } - ${ to }` }
+          />
+          <ListItemSecondaryAction>
+            <IconButton 
+              size="small" 
+              title={ strings.routes.deleteRoute } 
+              onClick={ () => this.onDeleteRouteClick(route) }
+            >
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      )
+    });
+  }
+
+  /**
+   * Formats and truncates a name string
+   * 
+   * @param name name string
+   * @param delimiter delimiter number
+   */
+  private truncateName = (name: string, delimiter: number ) => {
+    if (name.length <= delimiter) {
+      return name;
+    }
+    return `${ name.slice(0, delimiter) }...`;
+  }
+
+  /**
+   * List route item click handler click handler
+   *
+   * @param route route clicked
+   */
+  private onListItemClick = (route: Route) => {
+    const { onUserRouteSelect } = this.props;
+    onUserRouteSelect(route);
+  }
+
+  /**
+   * Initiate route deletion dialog click handler
+   *
+   * @param routeId route Id string
+   */
+  private onDeleteRouteClick = (route: Route) => {
+    this.setState({
+      locationDeleteInitiated: true,
+      locationToDelete: route 
+    });
+  }
+
+  /**
+   * Show more saved routes action handler
+   */
+  private onShowMoreClick = () => {
+    const { showAllUserLocations } = this.state;
+    this.setState({
+      showAllUserLocations: !showAllUserLocations
+    });
+  }
+
+  /**
+   * Delete confirm action handler
+   */
+  private onDeleteConfirm = () => {
+    const { onDeleteUserSavedRoute } = this.props;
+    const { locationToDelete } = this.state;
+    onDeleteUserSavedRoute(routeToDelete?.id!);
+    this.setState({
+      locationDeleteInitiated: false,
+      routeToDelete: undefined
+    })
+  }
+
+  /**
+   * Delete cancel action handler
+   */
+  private onDeleteCancel = () => {
+    this.setState({
+      routeDeleteInitiated: false,
+      routeToDelete: undefined
+    })
+  }
+}
+
+export default withStyles(styles)(SavedLocations);
---
+++ src/components/screens/saved-locations-screen/saved-locations-screen.tsx
@@ -0,0 +1,280 @@
+import React from "react";
+
+import Api from "../../../api";
+import { Dispatch } from "redux";
+import { connect } from "react-redux";
+import { styles } from "./saved-locations.styles";
+import { NullableToken } from "../../../types";
+import { Route } from "../../../generated/client";
+import * as actions from "../../../actions/route";
+import strings from "../../../localization/strings";
+import { ReduxActions, ReduxState } from "../../../store";
+import AppLayout from "../../layouts/app-layout/app-layout";
+import { Container, Card, CardContent, CardActions, withStyles, Button, Typography, WithStyles, CircularProgress } from "@material-ui/core";
+import ConfirmDialog from "../../generic/dialogs/confirm-dialog";
+import moment from "moment";
+
+/**
+ * Interface describing component props
+ */
+interface Props extends WithStyles<typeof styles> {}
+
+interface Props {
+  accessToken?: NullableToken;
+  keycloak?: Keycloak.KeycloakInstance;
+  updateDisplayedRoute: (displayedRoute: Route) => void;
+}
+
+/**
+ * Interface describing component state
+ */
+interface State {
+  deleteDialogVisible: boolean;
+  routes: Route[];
+  routeToDelete?: Route;
+  deletingRoute: boolean;
+  loadingRoutes: boolean;
+  redirect?: string;
+  error?: string | Error | Response;
+}
+
+/**
+ * Component for saved routes screen
+ */
+class SavedRoutesScreen extends React.Component<Props, State> {
+
+  /**
+   * Component constructor
+   * 
+   * @param props props
+   */
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      deleteDialogVisible: false,
+      routes: [],
+      deletingRoute: false,
+      loadingRoutes: false
+    };
+  }
+
+  /**
+   * Component life cycle method
+   */
+  public componentDidMount = async () => {
+    await this.updateRoutesList();
+  }
+
+  /**
+   * Component render method
+   */
+  public render() {
+    const { redirect, deleteDialogVisible, error } = this.state;
+    const { accessToken, keycloak, classes } = this.props;
+
+    return (
+      <AppLayout 
+        accessToken={ accessToken } 
+        keycloak={ keycloak } 
+        error={ error } 
+        clearError={ this.clearError } 
+        redirectTo={ redirect }>
+        <Container>
+          <Typography className={ classes.title } variant="h3" component="h1">
+            { strings.savedRoutes }
+          </Typography>
+          {
+            this.renderRouteCards()
+          }
+          <ConfirmDialog 
+            title={ strings.deleteConfirm } 
+            positiveButtonText={ strings.common.yes } 
+            cancelButtonText={ strings.common.cancel } 
+            dialogVisible={ deleteDialogVisible } 
+            onDialogConfirm={ this.deleteRoute } 
+            onDialogCancel={ this.closeDeleteDialog } />
+        </Container>
+      </AppLayout>
+    );
+  }
+
+  /**
+   * Renders route cards
+   * 
+   * @returns a rendered route cards
+   */
+  private renderRouteCards = () => {
+    const { classes } = this.props;
+    const { routes, loadingRoutes } = this.state;
+    if (!loadingRoutes) {
+      return (
+        <>
+          { routes.map(this.renderRouteCard) }
+        </>
+      );
+    }
+
+    return(
+      <CircularProgress className={ classes.primaryLoader } />
+    );
+  }
+
+  /**
+   * Renders a card for a route
+   * 
+   * @param route a route to render
+   * 
+   * @returns a rendered card
+   */
+  private renderRouteCard = (route: Route): JSX.Element => {
+    const { classes } = this.props;
+
+    return (
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="subtitle1" component="p">
+            { strings.savedRoutesFrom }: { route.locationFromName }
+              <br />
+            { strings.savedRoutesTo }: { route.locationToName }
+            </Typography>
+          <Typography variant="caption" component="p">
+            { strings.savedRoutesSavedText }: { this.dateToDMY(route.savedAt!) }
+            </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" color="primary" size="small" onClick={ () => this.displayRoute(route) }>{ strings.viewRoute }</Button>
+          <Button variant="contained" className={ classes.errorButton } size="small" onClick={ () => this.openDeleteDialog(route) }>{ strings.deleteRoute }</Button>
+        </CardActions>
+      </Card>
+    );
+  }
+
+  /**
+   * Display route method
+   *
+   * @param route route to display
+   */
+  private displayRoute = (route: Route) => {
+    this.props.updateDisplayedRoute(route);
+    this.setState({ redirect: "/map" });
+  } 
+
+  /**
+   * Converts Date to a localized string in DD-MMMM-YYYY format
+   * 
+   * @param date A date to convert
+   * 
+   * @returns A localized string in DD-MMMM-YYYY format
+   */
+  private dateToDMY = (date: Date): string => {
+    return moment(date).format("LL");
+  }
+
+  /**
+   * Deletes a route
+   */
+  private deleteRoute = async () => {
+    const { accessToken } = this.props;
+    const { routeToDelete } = this.state;
+
+    if (!accessToken || !routeToDelete) {
+      return;
+    }
+
+    this.setState({ deletingRoute: true });
+
+    try {
+      const routesApi = Api.getRoutesApi(accessToken);
+      await routesApi.deleteRoute({ routeId: routeToDelete.id! });
+      await this.updateRoutesList();
+      this.setState({ deleteDialogVisible: false });
+    } catch (error) {
+      this.setState({
+        error: error
+      });
+    }
+
+    this.setState({ deletingRoute: false });
+  }
+
+  /**
+   * Updates the list for routes
+   */
+  private updateRoutesList = async () => {
+    const { accessToken } = this.props;
+
+    if (!accessToken) {
+      return;
+    }
+
+    this.setState({ loadingRoutes: true });
+
+    try {
+      const routesApi = Api.getRoutesApi(accessToken);
+      const routes = await routesApi.listRoutes();
+      this.setState({ routes });
+    } catch (error) {
+      this.setState({
+        error: error
+      });
+    }
+
+    this.setState({ loadingRoutes: false });
+  }
+
+  /**
+   * Closes the delete dialog
+   * 
+   */
+  private closeDeleteDialog = () => {
+    this.setState({
+      deleteDialogVisible: false,
+      routeToDelete: undefined
+    });
+  }
+
+  /**
+   * Opens the delete dialog
+   * 
+   * @param routeToDelete a route to delete
+   */
+  private openDeleteDialog = (routeToDelete: Route) => {
+    this.setState({
+      deleteDialogVisible: true,
+      routeToDelete
+    });
+  }
+
+  /**
+   * Clears error
+   */
+  private clearError = () => {
+    this.setState({ 
+      error: undefined 
+    });
+  }
+}
+
+/**
+ * Redux mapper for mapping store state to component props
+ * @param state store state
+ */
+export function mapStateToProps(state: ReduxState) {
+  return {
+    accessToken: state.auth.accessToken,
+    keycloak: state.auth.keycloak
+  };
+}
+
+/**
+ * Redux mapper for mapping component dispatches 
+ * 
+ * @param dispatch dispatch method
+ */
+export function mapDispatchToProps(dispatch: Dispatch<ReduxActions>) {
+  return {
+    updateDisplayedRoute: (displayedRoute?: Route) => dispatch(actions.setDisplayedRoute(displayedRoute))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(SavedRoutesScreen));
